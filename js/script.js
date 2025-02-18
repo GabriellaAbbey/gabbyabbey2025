b80b8a9af2df04d6a6fc43554d4854f7014fd9c1
Gabby <156079426+GabriellaAbbey@users.noreply.github.com>
2025-02-17 20:53:35 -1000

    added in htangs

--- js/script.js
+++ js/script.js
@@ -18,3 +18,54 @@ toggleMenuLinks.forEach((el) => {
     toggleMenu.setAttribute('data-menustate','closed');
   }
 });
+// BACKGROUND COLOR CHANGE JS WITH NO SWIPER
+const sections = document.querySelectorAll('.scroll-section');
+const backgroundImage = document.querySelector('.background-image');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const slideNumber = entry.target.dataset.slide;
+            backgroundImage.setAttribute('data-currentslide', slideNumber);
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+// BACKGROUND COLOR CHANGE JS WITH SWIPER
+
+  
+// const swiper = new Swiper('.swiper-container', {
+//     direction: 'vertical',
+//     spaceBetween: 100,
+//     mousewheel: true,
+//     keyboard: {
+//         enabled: true,
+//         onlyInViewport: false
+//     },
+//     effect: 'slides',
+//     coverflowEffect: {
+//         rotate: 50,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 1,
+//         slideShadows: true,
+//     },
+//     speed: 1000,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+// });
+
+// const backgroundImage = document.querySelector(".background-image");
+
+// swiper.on('slideChange', function () {
+// console.log(swiper.activeIndex);
+// backgroundImage.setAttribute("data-currentslide", swiper.activeIndex);
+// });
